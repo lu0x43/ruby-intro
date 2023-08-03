73b38ee1dce5cdbf3cdf11dea6e67bb10d4fb5d3
LucasCaetano <lucas_caetano123@hotmail.com>
2023-08-03 00:13:00 -0300

    operadores e array, principios

--- first.rb
+++ first.rb
@@ -1,2 +1,32 @@
+# Hello World! pra dar sorte no primeiro programa em Ruby
 puts "Hello World!"
 p "Hello Lucas Caetano!"
+
+# Operations
+var = 10 
+var2 = 20
+
+puts var + var2
+#esperado 30
+
+puts var - var2
+#esperado -10
+
+puts var * var2
+#esperado 200
+
+puts var2 / var
+#esperado 2
+
+puts var2 % var
+#esperado 0
+
+# Arrays
+array = [1,2,3,4,5,6,7,8,9,10]
+# puts array #esperado [1,2,3,4,5,6,7,8,9,10]
+
+puts array[0]
+#esperado 1
+
+puts array[8]
+#esperado 9
